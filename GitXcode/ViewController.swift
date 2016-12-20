6b3758eb06a79c83837733457aaebf4d2347c97f
Monica Monteiro <monicamo@gmail.com>
2016-12-20 20:11:43 -0200

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Monica Monteiro on 20/12/16.
+//  Copyright © 2016 Monica Monteiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
