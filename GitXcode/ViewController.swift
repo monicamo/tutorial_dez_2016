d34831e4f3ce80455e1a59aacbad5dd32baa0682
Monica Monteiro <monicamo@gmail.com>
2016-12-20 20:55:58 -0200

    Implementei o botão de alert - Estória Hello World

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func alertarHelloWorld(_ sender: UIButton) {
+        
+        let alerta = UIAlertController(title: "Olá Mundo", message: "Oi mundo, espero que você melhore...", preferredStyle: .alert)
+        
+        alerta.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alerta, animated: true, completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
